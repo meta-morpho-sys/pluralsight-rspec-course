8c92ff5f0ea0f99c1d92b4afb50474ec36461c45
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-01-31 12:23:18 +0000

    Refactored the test using a helper method.

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -3,9 +3,14 @@ require 'card'
 
 describe 'hand rankings' do
   example 'hand with highest card wins' do
-    expect(HighCard.beats?([Card.build(:hearts, 10)], \
-                            [Card.build(:hearts, 9)])).to eq true
-    expect(HighCard.beats?([Card.build(:hearts, 9)], \
-                            [Card.build(:hearts, 10)])).to eq false
+    # Helper method. Given an array of strings it maps the strings and builds
+    # a card from them using the from string method.
+    def hand(strings)
+      strings.map { |x| Card.from_string(x) }
+    end
+    expect(HighCard.beats?(hand(%w[10H]), \
+                           hand(%w[9H]))).to eq true
+    expect(HighCard.beats?(hand(%w[9H]), \
+                           hand(%w[10H]))).to eq false
   end
 end
