3940228739b983c670780fb2d6b3047a414f4716
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-01-31 11:33:18 +0000

    Using a boolean boolean composition in Rspec.

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -23,7 +23,7 @@ describe 'CLI', :acceptance do
       sleep 0.5
       buffer = output.read_nonblock(1024)
       puts buffer
-      raise unless buffer.include?('You won') || buffer.include?('You lost')
+      expect(buffer).to include('You won').or include('You lost')
     end
   end
 end
